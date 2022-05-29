f6e73cbee146f8c434b65d10010d831dcf9d8cb5
phubuideptrai <btphu146@gmail.com>
2022-05-29 14:02:31 +0700

    fix

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, jsonify, request
 import time
 
 app = Flask(__name__)
-@app.route("/bot", method = ["POST"])
+@app.route('/bot', methods = ['POST'])
 
 #response
 def response(): 
-    query = dict(request.form)["query"]
-    result = query + " " + time.ctime()
+    query = dict(request.form)[        'query']
+    result = query + ' ' + time.ctime()
     return jsonify({"response": result})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",)
+    app.run(host='0.0.0.0',)
