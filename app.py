ab40ebbd7cf0002ee99022c1546dcc00d9f6f4e9
phubuideptrai <btphu146@gmail.com>
2022-05-29 16:28:22 +0700

    fix

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 
+# from flask import Flask, jsonify, request
+# import time
+
+# app = Flask(__name__)
+# @app.route("/bot", methods = ["POST"])
+
+# #response
+# def response(): 
+#     query = dict(request.form)['query']
+#     result = query + " " + time.ctime()
+#     return jsonify({"response": result})
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0",)
 from flask import Flask, jsonify, request
-import time
 
 app = Flask(__name__)
-@app.route("/bot", methods = ["POST"])
 
-#response
-def response(): 
+@app.route("/", methods = ["POST"])
+def response():
+ 
+
     query = dict(request.form)['query']
     result = query + " " + time.ctime()
     return jsonify({"response": result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",)
+    app.run()
